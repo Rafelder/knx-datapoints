4422daab704d36c69a92680045e3c4dacd26955e
Bram Roelandts <brammahh@gmail.com>
2018-08-03 12:07:16 +0200

    Corrected DPT6 and its tests.

--- tests/dpt006.spec.js
+++ tests/dpt006.spec.js
@@ -3,10 +3,10 @@ const knxDatapoints = require('../')
 
 suite('dpt6', () => {
   test('decode', () => {
-    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('00', 'hex')), -128)
-    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('64', 'hex')), -28)
-    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('C8', 'hex')), 72)
-    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('FF', 'hex')), 127)
+    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('80', 'hex')), -128)
+    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('E4', 'hex')), -28)
+    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('48', 'hex')), 72)
+    assert.strictEqual(knxDatapoints.decode('6.001', Buffer.from('7F', 'hex')), 127)
 
     assert.throws(() => { knxDatapoints.decode('6.001', Buffer.alloc(0)) }, Error)
     assert.throws(() => { knxDatapoints.decode('6.001', Buffer.alloc(2, 0)) }, Error)
@@ -14,10 +14,10 @@ suite('dpt6', () => {
   })
 
   test('encode', () => {
-    assert.ok(knxDatapoints.encode('6.001', -128).equals(Buffer.from('00', 'hex')))
-    assert.ok(knxDatapoints.encode('6.001', -28).equals(Buffer.from('64', 'hex')))
-    assert.ok(knxDatapoints.encode('6.001', 72).equals(Buffer.from('C8', 'hex')))
-    assert.ok(knxDatapoints.encode('6.001', 127).equals(Buffer.from('FF', 'hex')))
+    assert.ok(knxDatapoints.encode('6.001', -128).equals(Buffer.from('80', 'hex')))
+    assert.ok(knxDatapoints.encode('6.001', -28).equals(Buffer.from('E4', 'hex')))
+    assert.ok(knxDatapoints.encode('6.001', 72).equals(Buffer.from('48', 'hex')))
+    assert.ok(knxDatapoints.encode('6.001', 127).equals(Buffer.from('7F', 'hex')))
 
     assert.throws(() => { knxDatapoints.encode('6.001', -129) }, RangeError)
     assert.throws(() => { knxDatapoints.encode('6.001', 128) }, RangeError)
