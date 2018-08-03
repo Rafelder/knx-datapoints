82d0c0b4afb98cdab0533d1e52d22c1466547568
Bram Roelandts <dev@roelandts.cc>
2018-08-03 13:14:49 +0200

    Corrected the DPT6, DPT8 and DPT13 implementation. (#2)
    * Corrected the DPT13 implementation.
    
    * Fixed tests for DPT13.
    
    * Corrected DPT6 and its tests.
    
    * Corrected DPT8 and its tests.

--- tests/dpt008.spec.js
+++ tests/dpt008.spec.js
@@ -4,23 +4,23 @@ const knxDatapoints = require('../')
 suite('dpt8', () => {
   test('decode', () => {
     ['8.001', '8.002', '8.005', '8.006', '8.007', '8.011'].forEach((dpt) => {
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('0000', 'hex')), -32768)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('6464', 'hex')), -7068)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('C8C8', 'hex')), 18632)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('FFFF', 'hex')), 32767)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('8000', 'hex')), -32768)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('E464', 'hex')), -7068)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('48C8', 'hex')), 18632)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('7FFF', 'hex')), 32767)
     });
 
     ['8.003', '8.010'].forEach((dpt) => {
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('0000', 'hex')), -327.68)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('6464', 'hex')), -70.68)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('C8C8', 'hex')), 186.32)
-      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('FFFF', 'hex')), 327.67)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('FFE0', 'hex')), -320)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('FFF9', 'hex')), -70)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('0012', 'hex')), 180)
+      assert.strictEqual(knxDatapoints.decode(dpt, Buffer.from('0020', 'hex')), 320)
     })
 
-    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('0000', 'hex')), -3276.8)
-    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('6464', 'hex')), -706.8)
-    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('C8C8', 'hex')), 1863.2)
-    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('FFFF', 'hex')), 3276.7)
+    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('FFE0', 'hex')), -3200)
+    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('FFF9', 'hex')), -700)
+    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('0012', 'hex')), 1800)
+    assert.strictEqual(knxDatapoints.decode('8.004', Buffer.from('0020', 'hex')), 3200)
 
     assert.throws(() => { knxDatapoints.decode('8.001', Buffer.alloc(0)) }, Error)
     assert.throws(() => { knxDatapoints.decode('8.001', Buffer.alloc(3)) }, Error)
@@ -29,27 +29,27 @@ suite('dpt8', () => {
 
   test('encode', () => {
     ['8.001', '8.002', '8.005', '8.006', '8.007', '8.011'].forEach((dpt) => {
-      assert.ok(knxDatapoints.encode(dpt, -32768).equals(Buffer.from('0000', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, -7068).equals(Buffer.from('6464', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, 18632).equals(Buffer.from('C8C8', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, 32767).equals(Buffer.from('FFFF', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, -32768).equals(Buffer.from('8000', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, -7068).equals(Buffer.from('E464', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, 18632).equals(Buffer.from('48C8', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, 32767).equals(Buffer.from('7FFF', 'hex')))
       assert.throws(() => { knxDatapoints.encode(dpt, -32769) }, RangeError)
       assert.throws(() => { knxDatapoints.encode(dpt, 32768) }, RangeError)
     });
 
     ['8.003', '8.010'].forEach((dpt) => {
-      assert.ok(knxDatapoints.encode(dpt, -327.68).equals(Buffer.from('0000', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, -70.68).equals(Buffer.from('6464', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, 186.32).equals(Buffer.from('C8C8', 'hex')))
-      assert.ok(knxDatapoints.encode(dpt, 327.67).equals(Buffer.from('FFFF', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, -327.68).equals(Buffer.from('FFE0', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, -70.68).equals(Buffer.from('FFF9', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, 186.32).equals(Buffer.from('0012', 'hex')))
+      assert.ok(knxDatapoints.encode(dpt, 327.67).equals(Buffer.from('0020', 'hex')))
       assert.throws(() => { knxDatapoints.encode(dpt, -327.69) }, RangeError)
       assert.throws(() => { knxDatapoints.encode(dpt, 327.68) }, RangeError)
     })
 
-    assert.ok(knxDatapoints.encode('8.004', -3276.8).equals(Buffer.from('0000', 'hex')))
-    assert.ok(knxDatapoints.encode('8.004', -706.8).equals(Buffer.from('6464', 'hex')))
-    assert.ok(knxDatapoints.encode('8.004', 1863.2).equals(Buffer.from('C8C8', 'hex')))
-    assert.ok(knxDatapoints.encode('8.004', 3276.7).equals(Buffer.from('FFFF', 'hex')))
+    assert.ok(knxDatapoints.encode('8.004', -3276.8).equals(Buffer.from('FFE0', 'hex')))
+    assert.ok(knxDatapoints.encode('8.004', -706.8).equals(Buffer.from('FFF9', 'hex')))
+    assert.ok(knxDatapoints.encode('8.004', 1863.2).equals(Buffer.from('0012', 'hex')))
+    assert.ok(knxDatapoints.encode('8.004', 3276.7).equals(Buffer.from('0020', 'hex')))
     assert.throws(() => { knxDatapoints.encode('8.004', -3276.9) }, RangeError)
     assert.throws(() => { knxDatapoints.encode('8.004', 3276.8) }, RangeError)
   })
--- tests/dpt013.spec.js
+++ tests/dpt013.spec.js
@@ -3,8 +3,8 @@ const knxDatapoints = require('../')
 
 suite('dpt13', () => {
   test('decode', () => {
-    assert.strictEqual(knxDatapoints.decode('13.001', Buffer.from('00000000', 'hex')), -2147483648)
-    assert.strictEqual(knxDatapoints.decode('13.001', Buffer.from('FFFFFFFF', 'hex')), 2147483647)
+    assert.strictEqual(knxDatapoints.decode('13.001', Buffer.from('0000000A', 'hex')), 10)
+    assert.strictEqual(knxDatapoints.decode('13.001', Buffer.from('FFFFFFF6', 'hex')), -10)
 
     assert.throws(() => { knxDatapoints.decode('13.001', Buffer.alloc(0)) }, Error)
     assert.throws(() => { knxDatapoints.decode('13.001', Buffer.alloc(5)) }, Error)
@@ -12,8 +12,8 @@ suite('dpt13', () => {
   })
 
   test('encode', () => {
-    assert.ok(knxDatapoints.encode('13.001', -2147483648).equals(Buffer.from('00000000', 'hex')))
-    assert.ok(knxDatapoints.encode('13.001', 2147483647).equals(Buffer.from('FFFFFFFF', 'hex')))
+    assert.ok(knxDatapoints.encode('13.001', 10).equals(Buffer.from('0000000A', 'hex')))
+    assert.ok(knxDatapoints.encode('13.001', -10).equals(Buffer.from('FFFFFFF6', 'hex')))
 
     assert.throws(() => knxDatapoints.encode('13.001', -2147483649), RangeError)
     assert.throws(() => knxDatapoints.encode('13.001', 2147483648), RangeError)
