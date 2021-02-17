3bf2ec75001e144683bd0ac3c9f8867e2e376e04
Timo Müller <timomueller1993@googlemail.com>
2021-02-17 11:28:00 +0100

    fixed month conversion in DPT 11 (#5)

--- tests/dpt011.spec.js
+++ tests/dpt011.spec.js
@@ -7,18 +7,23 @@ suite('dpt11', () => {
 
     decoded = knxDatapoints.decode('11.001', Buffer.from('010100', 'hex'))
     assert.strictEqual(decoded.getFullYear(), 2000)
-    assert.strictEqual(decoded.getMonth(), 1)
+    assert.strictEqual(decoded.getMonth(), 0)
     assert.strictEqual(decoded.getDate(), 1)
 
     decoded = knxDatapoints.decode('11.001', Buffer.from('01015A', 'hex'))
     assert.strictEqual(decoded.getFullYear(), 1990)
-    assert.strictEqual(decoded.getMonth(), 1)
+    assert.strictEqual(decoded.getMonth(), 0)
     assert.strictEqual(decoded.getDate(), 1)
 
-    decoded = knxDatapoints.decode('11.001', Buffer.from('050505', 'hex'))
+    decoded = knxDatapoints.decode('11.001', Buffer.from('050605', 'hex'))
     assert.strictEqual(decoded.getFullYear(), 2005)
     assert.strictEqual(decoded.getMonth(), 5)
     assert.strictEqual(decoded.getDate(), 5)
+
+    decoded = knxDatapoints.decode('11.001', Buffer.from('010115', 'hex'))
+    assert.strictEqual(decoded.getFullYear(), 2021)
+    assert.strictEqual(decoded.getMonth(), 0)
+    assert.strictEqual(decoded.getDate(), 1)
 
     assert.throws(() => { knxDatapoints.decode('11.001', Buffer.alloc(0)) }, Error)
     assert.throws(() => { knxDatapoints.decode('11.001', Buffer.alloc(4)) }, Error)
@@ -26,20 +31,21 @@ suite('dpt11', () => {
   })
 
   test('encode', () => {
-    assert.ok(knxDatapoints.encode('11.001', new Date(2000, 1, 1)).equals(Buffer.from('010100', 'hex')))
-    assert.ok(knxDatapoints.encode('11.001', new Date(1990, 1, 1)).equals(Buffer.from('01015A', 'hex')))
-    assert.ok(knxDatapoints.encode('11.001', new Date(2005, 5, 5)).equals(Buffer.from('050505', 'hex')))
+    assert.ok(knxDatapoints.encode('11.001', new Date(2000, 0, 1)).equals(Buffer.from('010100', 'hex')))
+    assert.ok(knxDatapoints.encode('11.001', new Date(1990, 0, 1)).equals(Buffer.from('01015A', 'hex')))
+    assert.ok(knxDatapoints.encode('11.001', new Date(2005, 4, 5)).equals(Buffer.from('050505', 'hex')))
+    assert.ok(knxDatapoints.encode('11.001', new Date(2021, 0, 1)).equals(Buffer.from('010115', 'hex')))
 
     const today = new Date()
     const expectedBuffer = Buffer.from([
       today.getDate(),
-      today.getMonth(),
+      today.getMonth() + 1,
       today.getFullYear() - 2000
     ])
     assert.ok(knxDatapoints.encode('11.001', undefined).equals(expectedBuffer))
     assert.throws(() => { knxDatapoints.encode('11.001', null) }, TypeError)
 
-    assert.throws(() => { knxDatapoints.encode('11.001', new Date(1989, 1, 1)) }, RangeError)
-    assert.throws(() => { knxDatapoints.encode('11.001', new Date(2090, 1, 1)) }, RangeError)
+    assert.throws(() => { knxDatapoints.encode('11.001', new Date(1989, 0, 1)) }, RangeError)
+    assert.throws(() => { knxDatapoints.encode('11.001', new Date(2090, 0, 1)) }, RangeError)
   })
 })
